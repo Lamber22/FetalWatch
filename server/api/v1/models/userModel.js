11f8b8a2effbbec67fdb4157f8af166605b53048
Lamber22 <Lamberjoe5@gmail.com>
2024-08-02 00:36:40 +0000

    update

--- server/api/v1/models/userModel.js
+++ server/api/v1/models/userModel.js
@@ -3,21 +3,27 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    "firstName": { type: String, require: true },
-    "lastName": { type: String, require: true },
-    "email": { type: String, require: true, unique: true },
-    "password": { type: String, require: true },
-    "role": {
+    firstName: {
+        type: String, require: true
+    },
+    lastName: {
+        type: String, require: true
+    },
+    email: {
+        type: String, require: true, unique: true
+    },
+    password: {
+        type: String, require: true
+    },
+    role: {
         type: String, enum: ["admin", "healthProvider", "doctor", "midwife"],
         default: "healthProvider", require: true
     },
     createdAt: {
-        type: Date,
-        default: Date.now
+        type: Date, default: Date.now
     },
     updatedAt: {
-        type: Date,
-        default: Date.now
+        type: Date, default: Date.now
     },
     createdBy: { type: Schema.Types.ObjectId, ref: 'User' }, // Reference to the admin who created the user
 });
