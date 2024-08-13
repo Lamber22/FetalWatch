fd4bb28e396721d091b5f08f94759475b55fc9e6
Lamber22 <Lamberjoe5@gmail.com>
2024-08-13 13:24:35 +0000

    middleware created

---
+++ server/api/v1/utils/authMiddleware.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+const secretKey = process.env.JWT_SECRET;
+
+// Middleware to authenticate user
+const authMiddleware = (req, res, next) => {
+    const token = req.header("Authorization")?.split(" ")[1];
+    if (!token) {
+        return res
+        .status(401)
+        .json({
+            status: "failed",
+            message: "Authorization denied"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error("Error verifying token:", error);
+        res.status(401).json({ message: "Token is not valid" });
+    }
+};
+
+const jsonParserMiddleware = (req, res, next) => {
+    if (req.method === 'GET') {
+        return next(); // Skip JSON parsing for GET requests
+    }
+    return express.json()(req, res, next); // Parse JSON payload for non-GET requests
+};
+
+
+export { authMiddleware, checkRole, jsonParserMiddleware };
