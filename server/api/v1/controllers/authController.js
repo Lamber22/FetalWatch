f4e75907b73df038aa08a7fb5b1c187b86e2e61c
Lamber22 <Lamberjoe5@gmail.com>
2024-08-16 02:20:10 +0000

    Add email function and update authController

--- server/api/v1/controllers/authController.js
+++ server/api/v1/controllers/authController.js
@@ -68,3 +68,68 @@ export const signoutUser = async (req, res) => {
         .json({ error: "Server error", errorMessage: error.message });
     }
 };
+
+// forgot password handler
+export const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+        return res
+            .status(404)
+            .json({ status: "failed", message: "User not found" });
+        }
+
+        const resetToken = generateToken(user._id, user.role, { expiresIn: "1h" });
+        const resetUrl = `${process.env.RESET_PWD_URL}/${resetToken}`;
+
+        const emailOptions = {
+        from: process.env.EMAIL_USER,
+        email: user.email,
+        subject: "Password Reset",
+        content: `<p>You requested to reset your password. Click this link to reset your password:</p> <a href="${resetUrl}">${resetUrl}</a>`,
+        };
+
+        await sendEmail(emailOptions);
+
+        res.status(200).json({
+        status: "success",
+        message: "Password reset email sent successfully",
+        });
+    } catch (error) {
+        res
+        .status(500)
+        .json({ error: "Server error", errorMessage: error.message });
+    }
+};
+
+// reset password handler
+export const resetPassword = async (req, res) => {
+    const { token } = req.params;
+    const { password, confirmPassword } = req.body;
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decodedToken.id);
+
+        if (!user) {
+        return res.status(404).json({ message: "User not found" });
+        }
+
+        if (password !== confirmPassword) {
+        return res.status(400).json({ message: "Passwords do not match" });
+        }
+
+        user.password = password;
+        await user.save();
+
+        res.status(200).json({ message: "Password reset successfully" });
+    } catch (error) {
+        res
+        .status(500)
+        .json({ error: "Server error", errorMessage: error.message });
+    }
+};
