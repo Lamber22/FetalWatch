b6e0c075e06e49863f437f164916f9f53060f9b2
Lamber22 <Lamberjoe5@gmail.com>
2024-08-16 15:57:04 +0000

    feat: Add cors middleware

--- server/api/v1/app.js
+++ server/api/v1/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import dbClient from "./config/db.js";
 import authRouter from "./routes/authRoute.js";
 import userRouter from "./routes/userRoute.js";
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello from FetalWatch API" });
 });
 app.use(morgan("dev"));
+app.use(cors());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/patients", patientRouter);
